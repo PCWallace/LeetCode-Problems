59f7b52d64a3e0312e3b6e91e71349c94d234b7d
PCWallace <pratyushchhibber@gmail.com>
2020-01-19 11:44:25 +0530

    find the middle element

---
+++ LinkedList/MiddleElement.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	/**
+	We will use two pointer approach , in which one will be running two times faster, so that slow one will be at mid when fast one will 		finish the race
+	**/
+	public ListNode middleNode(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+}
